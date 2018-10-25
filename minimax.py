1f6af23fc0494cc8e547a43af9f3728265080510
Pseudoless <julienlhoest@hotmail.com>
2018-10-25 12:47:07 +0200

    Remove contribution variable and mindist fct

--- minimax.py
+++ minimax.py
@@ -40,7 +40,7 @@ class PacmanAgent(Agent):
             sentinel = -math.inf
 
             for son in sons:
-                val = self.minimax(son[0], True, 0, list(), list())
+                val = self.minimax(son[0], True, list(), list())
                 if val > sentinel:
                     sentinel = val
                     ret = son[1]
@@ -48,10 +48,9 @@ class PacmanAgent(Agent):
             self.computed.update({key: ret})
             return ret  # Value associated to key (position, food)
 
-    def minimax(self, state, PacmanTurn, contributions,
-                visited_Pacman, visited_ghost):
+    def minimax(self, state, PacmanTurn, visited_Pacman, visited_ghost):
         if state.isWin() or state.isLose():
-            return state.getScore() + contributions
+            return state.getScore()
 
         key = (state.getPacmanPosition(),
                tuple(state.getGhostPositions()), state.getFood())
@@ -68,13 +67,10 @@ class PacmanAgent(Agent):
                 if key_son in visited_Pacman:
                     continue
 
-                contribution = - self.mindist(son[0].getPacmanPosition(),
-                                              son[0].getFood())
                 new_visited_Pacman = visited_Pacman.copy()
                 new_visited_Pacman.append(key)
                 new_visited_ghost = visited_ghost.copy()
                 gameSum = self.minimax(son[0], False,
-                                       contributions + contribution,
                                        new_visited_Pacman, new_visited_ghost)
                 maxGameSum = max((maxGameSum, gameSum))
             return maxGameSum
@@ -95,39 +91,6 @@ class PacmanAgent(Agent):
                 new_visited_ghost = visited_ghost.copy()
                 new_visited_ghost.append(key)
                 gameSum = self.minimax(son[0], True,
-                                       contributions,
                                        new_visited_Pacman, new_visited_ghost)
                 minGameSum = min((minGameSum, gameSum))
             return minGameSum
-
-    def mindist(self, pos, foods):
-        """
-        Given a Pacman position and a food matrix, returns the shortest
-        distance to a dot.
-        Arguments:
-        ----------
-        - `pos`: Pacman's position as a pair (x,y) : x,y >= 0
-        - `food`: a matrix of booleans indicating by a True value the presence
-        of a dot in the maze.
-
-        Return:
-        -------
-        - A integer representing the longest distance to a dot
-        """
-        foods_pos = []
-        i = 0  # abscisses values
-        for rows in foods:
-            j = 0  # ordinates values
-            for elem in rows:
-                if elem:
-                    foods_pos.append((i, j))
-                j += 1
-            i += 1
-
-        distances = list(map(lambda x: abs(pos[0] - x[0]) + abs(pos[1] - x[1]),
-                             foods_pos))
-
-        if len(distances) == 0:
-            return 0
-
-        return min(distances)
